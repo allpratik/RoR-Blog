66a47ccb644ceabed851b2cefdaefc087ab59724
Pratik Khandagale <pratikk007@gmail.com>
2015-04-16 19:50:46 +0530

    Added Comments and Authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate, except: [:index, :show]
 
   # GET /posts
   # GET /posts.json
@@ -66,6 +67,12 @@ class PostsController < ApplicationController
     def set_post
       @post = Post.find(params[:id])
     end
+
+    def authenticate 
+        authenticate_or_request_with_http_basic do |name, password|
+          name == "admin"  && password == "secret"
+        end
+    end  
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
